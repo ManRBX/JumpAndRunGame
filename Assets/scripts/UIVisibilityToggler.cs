e7c2093dafa76b89eeb8ff0676da9218c63bd2e7
Manuel <55001894+ManRBX@users.noreply.github.com>
2025-05-18 20:57:45 +0200

    init update
    Level 10 is finished, a new function has been added so that you can now hide the UI and fade in and bonus levels have been added but not yet started to build

---
+++ Assets/scripts/UIVisibilityToggler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class UIVisibilityToggler : MonoBehaviour
+{
+    [Header("Alle UI-Elemente, die ein-/ausblendbar sein sollen")]
+    public GameObject[] uiElements;
+
+    [Header("Taste zum Umschalten")]
+    public KeyCode toggleKey = KeyCode.F1;
+
+    private bool uiVisible = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(toggleKey))
+        {
+            uiVisible = !uiVisible;
+
+            foreach (GameObject ui in uiElements)
+            {
+                if (ui != null)
+                    ui.SetActive(uiVisible);
+            }
+
+            Debug.Log("🔁 UI " + (uiVisible ? "eingeblendet" : "ausgeblendet"));
+        }
+    }
+}
