27032c819d0103d4e9fd16d84d72db485392c461
Manuel <55001894+ManRBX@users.noreply.github.com>
2026-03-30 16:47:16 +0200

    init update

---
+++ Assets/scripts/ExportSpritesToPNG.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class ExportSpritesToPNG
+{
+    [MenuItem("Tools/Export Sprites To PNG")]
+    public static void Export()
+    {
+        string folder = "Assets/ExportedSprites/";
+        if (!Directory.Exists(folder))
+            Directory.CreateDirectory(folder);
+
+        Object[] selected = Selection.objects;
+
+        int count = 0;
+
+        foreach (Object obj in selected)
+        {
+            string path = AssetDatabase.GetAssetPath(obj);
+            Object[] assets = AssetDatabase.LoadAllAssetsAtPath(path);
+
+            foreach (Object asset in assets)
+            {
+                if (asset is Sprite sprite)
+                {
+                    Texture2D tex = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+
+                    Color[] pixels = sprite.texture.GetPixels(
+                        (int)sprite.rect.x,
+                        (int)sprite.rect.y,
+                        (int)sprite.rect.width,
+                        (int)sprite.rect.height);
+
+                    tex.SetPixels(pixels);
+                    tex.Apply();
+
+                    byte[] bytes = tex.EncodeToPNG();
+                    File.WriteAllBytes(folder + sprite.name + ".png", bytes);
+
+                    count++;
+                }
+            }
+        }
+
+        AssetDatabase.Refresh();
+        Debug.Log("🔥 Export fertig! Anzahl: " + count);
+    }
+}
---
+++ Assets/scripts/HighResScreenshot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class HighResScreenshot : MonoBehaviour
+{
+    public int width = 1920;
+    public int height = 622;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            string folder = Application.dataPath + "/Screenshots/";
+
+            // Ordner erstellen wenn nicht vorhanden
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+
+            string fileName = "steam_banner_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+            string path = folder + fileName;
+
+            ScreenCapture.CaptureScreenshot(path);
+
+            Debug.Log("🔥 Screenshot gespeichert: " + path);
+        }
+    }
+}
